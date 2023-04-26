f92c9daf4e3b28f366b7c8e6b889e7757af16f2a
Vladimir Leushkin <Vleyshkin@yandex.ru>
2023-04-26 21:21:20 +0300

    feat: maven project

---
+++ src/main/java/com/example/taskfoxcom/TaskFoxcomApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskfoxcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFoxcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFoxcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskfoxcom/TaskFoxcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskfoxcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFoxcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
